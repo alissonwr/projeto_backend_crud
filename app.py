96fc54246b100fbb5628c5830fd4b08deb7cd459
Alisson <alisson-wendel@hotmail.com>
2023-10-22 11:33:20 -0300

    Novo: criação do arquivo index.js e atualização dos arquivos app.py, repositório.py, style.css, base.html, cadastro.html e index.html

--- app.py
+++ app.py
@@ -8,46 +8,46 @@ app = Flask(__name__)
 # Ao enviar dados para o servidor, a máquina do cliente usa um POST
 
 #É preciso criar rotas que levem em conta as seguintes funcionalidades:
-#Listar todos os produtos no template index.html
+#Listar todos os personagens no template index.html
 @app.route("/")
-def exibir_produtos():
-    return render_template('index.html', produtos = repositorio.retornar_produtos())
-#Abrir um produto específico (carregando seus dados) no template cadastro.html
-@app.route("/produto/<int:id>", methods=['GET', 'POST'])
-def exibir_produto(id):
+def exibir_personagens():
+    return render_template('index.html', personagens = repositorio.retornar_personagens())
+#Abrir um personagem específico (carregando seus dados) no template cadastro.html
+@app.route("/personagem/<int:id>", methods=['GET', 'POST'])
+def exibir_personagem(id):
 
     if request.method == 'POST':
     #Quer dizer que o usuário quer gravar um dado
         if "excluir" in request.form:
-           repositorio.remover_produto(id)
-           return redirect(url_for('exibir_produtos'))
+           repositorio.remover_personagem(id)
+           return redirect(url_for('exibir_personagens'))
         elif "salvar" in request.form: 
-            produto = {}
-            produto['nome'] = request.form["nome"]
-            produto['descricao'] = request.form["descricao"]
-            produto['casa'] = request.form["casa"]
-            produto['imagem'] = request.form["imagem"]
+            personagem = {}
+            personagem['nome'] = request.form["nome"]
+            personagem['descricao'] = request.form["descricao"]
+            personagem['casa'] = request.form["casa"]
+            personagem['imagem'] = request.form["imagem"]
 
-            if id in repositorio.retornar_produtos().keys():
-               repositorio.atualizar_produto(id, produto)
+            if id in repositorio.retornar_personagens().keys():
+               repositorio.atualizar_personagem(id, personagem)
 
-            return redirect(url_for('exibir_produtos'))
+            return redirect(url_for('exibir_personagens'))
     else:
     #Retorna os dados de um personagem na página de cadastro
-     produto = repositorio.retornar_produto(id)
-     produto['id'] = id
-    return render_template('cadastro.html', **produto)
+     personagem = repositorio.retornar_personagem(id)
+     personagem['id'] = id
+    return render_template('cadastro.html', **personagem)
 
 @app.route("/personagem", methods=["GET", "POST"])
-def criar_produto():
+def criar_personagem():
    if request.method == "POST":
-            produto = {}
-            produto['nome'] = request.form["nome"]
-            produto['descricao'] = request.form["descricao"]
-            produto['casa'] = request.form["casa"]
-            produto['imagem'] = request.form["imagem"]
-            repositorio.criar_produto(**produto)
-            return redirect(url_for('exibir_produtos'))
+            personagem = {}
+            personagem['nome'] = request.form["nome"]
+            personagem['descricao'] = request.form["descricao"]
+            personagem['casa'] = request.form["casa"]
+            personagem['imagem'] = request.form["imagem"]
+            repositorio.criar_personagem(**personagem)
+            return redirect(url_for('exibir_personagens'))
    else:
       return render_template('cadastro.html', id=repositorio.gerar_id())
 #Abrir o template cadastro.html apenas com o id preenchido para permitir novo cadastro
@@ -61,29 +61,3 @@ app.run(debug=True)
 
 
 
-# from flask import Flask, render_template
-# import repositorio
-
-# app = Flask(__name__)
-
-# #LEMBRE-SE -> 
-# # Ao obter dados do servidor, a máquina do cliente usa um GET
-# # Ao enviar dados para o servidor, a máquina do cliente usa um POST
-
-# #É preciso criar rotas que levem em conta as seguintes funcionalidades:
-# #Listar todos os produtos no template index.html
-# @app.route("/")
-# def exibir_produtos():
-#     return render_template('index.html', produtos = repositorio.retornar_produtos())
-# #Abrir um produto específico (carregando seus dados) no template cadastro.html
-# @app.route("/produto/<int:id>")
-# def exibir_produto(id):
-#     produto = repositorio.retornar_produto(id)
-#     produto['id'] = id
-#     return render_template('cadastro.html', **produto)
-# #Abrir o template cadastro.html apenas com o id preenchido para permitir novo cadastro
-# #Dar função aos botões excluir e salvar no template cadastro.html
-    
-
-
-# app.run(debug=True)
